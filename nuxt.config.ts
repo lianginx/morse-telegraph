81942a612f84bddcfae11979b5084a8c3f8ed668
Liang INX <liang@in-x.cc>
2023-12-19 09:09:40 +0800

    fix: cloudflare deploy fail

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,9 @@ export default defineNuxtConfig({
     transformers: [transformerVariantGroup()],
   },
   ssr: false,
+  vite: {
+    resolve: {
+      mainFields: ['main', 'browser'],
+    },
+  },
 })
